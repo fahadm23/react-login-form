fb96b6b99d5630666cf52ce03a3a20b6761cf6dc
Fahad Mohammad <fahad.mohammad06@gmail.com>
2022-07-26 05:13:16 -0400

    Completed react login form

---
+++ src/components/LoginForm.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+function LoginForm({ Login, error }) {
+  const [details, setDetails] = useState({ name: "", email: "", password: "" });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    Login(details);
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="form-inner">
+        <h2>
+          React Login Form<i class="fa-brands fa-react"></i>{" "}
+          <span className="info">
+            <ul>
+              <li>Use any name</li>
+              <li>Email: admin@admin.com</li>
+              <li>Password: admin123</li>
+            </ul>
+          </span>
+        </h2>
+        {error != "" ? <div className="error">{error}</div> : ""}
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => setDetails({ ...details, name: e.target.value })}
+            value={details.name}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setDetails({ ...details, email: e.target.value })}
+            value={details.email}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
+        </div>
+        <input type="submit" value="LOGIN" />
+      </div>
+    </form>
+  );
+}
+
+export default LoginForm;
